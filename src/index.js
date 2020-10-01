19405e9568c4e416adff2ab8a312df31c421175a
Freshaku <52348428+Freshaku@users.noreply.github.com>
2020-10-01 18:19:51 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if ((array || []).length === 0){
+        return 0;
+    }
+
+    let min = Math.min.apply(null, array);
+    return min;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+    if ((array || []).length === 0){
+        return 0;
+    }
+    let max = Math.max.apply(null, array);
+    return max;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+    if ((array || []).length === 0){
+        return 0;
+    }
+
+    let sum = array[0];
+    let average = 0;
+    for(let i = 1; i < array.length; i++){
+        sum += array[i];  
+        average = sum / array.length;
+    }
+    return average;
+
 }
